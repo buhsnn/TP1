5f31aab608daf47483dd68c102cca02d87a7778f
buhsnn <bushra.hossain@efrei.net>
2025-05-22 09:43:04 +0200

    back end api endpoint

--- backend-api/src/main/java/fr/takima/training/simpleapi/SimpleApiApplication.java
+++ backend-api/src/main/java/fr/takima/training/simpleapi/SimpleApiApplication.java
@@ -1,13 +1,13 @@
-package com.example.java_docker;
+package fr.takima.training.simpleapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaDockerApplication {
+public class SimpleApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaDockerApplication.class, args);
+		SpringApplication.run(SimpleApiApplication.class, args);
 	}
 
 }
